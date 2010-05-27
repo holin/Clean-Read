9fe5a564dff1c2c6272b03ec031dc2fd95acb55c
HoLin <holin.he@gmail.com>
2010-05-27 16:04:25 +0800

    .iresearch.cn

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 $.getScript("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js", function(){
-	// $.getScript("http://github.com/holin/Clean-Read/raw/master/cleanread.js");
-	$.getScript("http://localhost:8000/cleanread.js?"+(new Date())); 	
+	$.getScript("http://github.com/holin/Clean-Read/raw/master/cleanread.js");
+	// $.getScript("http://localhost:8000/cleanread.js?"+(new Date())); 	
 }); 
